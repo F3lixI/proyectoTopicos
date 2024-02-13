f04bddfb760ea0fe57dd29b747bdf9de8762069a
Felix <gera17_o4@hotmail.com>
2024-02-12 21:22:17 -0700

    extraccion de productos y llenado de  tablas

--- Venta/views.py
+++ Venta/views.py
@@ -4,8 +4,8 @@ import requests, json
 
 
 
-def welcome(request):
-    url = "https://www.floristone.com/api/rest/flowershop/welcome"
+def index(request):
+    url = "https://www.floristone.com/api/rest/flowershop/getproducts?category=lr&count=1&start=1"
     payload = {}
     headers = {
       'Authorization': 'Basic NTkzOTE5Okhpc0VFbQ==',
@@ -36,28 +36,3 @@ def flower_detail(request):
         
     return render(request, 'flower_detail.html', {'flowers': flowers})
 
-def index(request):
-    products = [
-        {
-            "CODE": "F1-509",
-            "PRICE": 74.95,
-            "RECIPIENT": {
-                "ZIPCODE": "11779"
-            }
-        }
-    ]
-    
-    # Codificar la lista de productos en JSON
-    products_json = json.dumps(products)
-    
-    # Construir la URL con los datos JSON codificados
-    url = f"https://www.floristone.com/api/rest/flowershop/gettotal?products={products_json}"
-    
-    headers = {
-        'Authorization': 'Basic NTkzOTE5Okhpc0VFbQ==',
-        'Cookie': 'CFID=49269679; CFTOKEN=da0434cbd367fbc8-D8BBFBE5-E0A4-F32B-6374406A26AE16E7; JSESSIONID=03CA41F72A86539F70B7400157C72110.cfusion'
-    }
-    
-    response = requests.get(url, headers=headers)
-    
-    return JsonResponse(response.json())
