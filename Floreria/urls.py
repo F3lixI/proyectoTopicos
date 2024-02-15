ca23e81ae92c1cff022ea5a1268ee75dd92dda15
Felix <gera17_o4@hotmail.com>
2024-02-14 19:37:59 -0700

    homepage

--- Floreria/urls.py
+++ Floreria/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('venta/', include('Venta.urls')) 
+    path('', include('Venta.urls')) 
 ]
--- Venta/urls.py
+++ Venta/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("flower_detail", views.flower_detail, name="flower_detail"),
+    path('', views.index, name='index'),
 ]
 
