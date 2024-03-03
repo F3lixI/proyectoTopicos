f598e3c604e4dfebcf8a8c176d85eaea4643241c
Felix <gera17_o4@hotmail.com>
2024-03-03 16:47:23 -0700

    index

--- todo/views.py
+++ todo/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
 
-from .models import ToDo
-
 
 def index(request):
     
-    if request.method == "POST":
-        print("POST")
-        text = request.POST.get("text", None)
-        print("TEXT", text)
-        if text:
-            print("SAVE")
-            ToDo.objects.create(
-                text=text
-            )
 
-    todo = ToDo.objects.all()
-
-    return render(request, "index.html", {"todo": todo})
+    return render(request, "index.html")
