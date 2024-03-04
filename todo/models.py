63c903144e3c284227b2a44170291424ab648bbf
Felix <gera17_o4@hotmail.com>
2024-03-03 22:14:25 -0700

    index

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+#Clase mostrada en el ejemplo
 class ToDo(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(
@@ -13,3 +13,20 @@ class ToDo(models.Model):
 
     def __str__(self) -> str:
         return f"{self.text}"
+    
+class Flores(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=50)
+    small_image = models.URLField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    dimension = models.CharField(max_length=20)
+    large_image = models.URLField()
+    extra_large_image = models.URLField()
+    service = models.CharField(max_length=1)
+    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import Flores
 
 def index(request):
     
+    flores = Flores.objects.all()[:5]
+    
+    plantas = Flores.objects.all()[5:10]
+    
+    return render(request, 'index.html', {'flores': flores, 'plantas': plantas})
+    
 
-    return render(request, "index.html")
 
