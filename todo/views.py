519d8d3d472604a0bac8fee253bc7176aeeb3e06
Felix <gera17_o4@hotmail.com>
2024-03-04 11:09:05 -0700

    login

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from . models import Flores
 from .forms import CustomCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.db.models import Q
 
 def index(request):
     
@@ -20,3 +23,24 @@ def signup(request):
         form = CustomCreationForm()
     
     return render(request, 'signup.html', {'form': form})
+
+def iniciarSesion(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+        
+        return render(request, 'login.html', {'form': form})
+                
+    else:
+        # Si no es una solicitud POST, simplemente renderiza el formulario vacío
+        return render(request, 'login.html', {'form': AuthenticationForm()})
+    
