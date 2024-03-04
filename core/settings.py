52847070ddb14c98dae016a834497e5b57514140
Felix <gera17_o4@hotmail.com>
2024-03-03 22:07:46 -0700

    database

--- core/settings.py
+++ core/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'floreriadb',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '4uFbwWAVjmc+'
     }
 }
 
